eca34deaa350211f92a879b887c7a32f4df683c3
Nick Rizk <nrizk6@gmail.com>
2020-05-12 19:39:12 -0400

    word count now in menu bar with basic persistence

--- PlayingWithNSEvents/AppDelegate.swift
+++ PlayingWithNSEvents/AppDelegate.swift
@@ -12,18 +12,52 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
     
     var statusBarItem: NSStatusItem!
-
+    var currentWordCount: Int!
+    static let currentWordCountKey = "currentWordCount"
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         
-        let statusBar = NSStatusBar.system
-        statusBarItem = statusBar.statusItem(withLength: NSStatusItem.squareLength)
-        statusBarItem.button?.title = "✍"
+        currentWordCount = UserDefaults.standard.integer(forKey: AppDelegate.currentWordCountKey)
+        
+        statusBarItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+        statusBarItem.button?.title = "\(currentWordCount!)"
+        
         let statusBarMenu = NSMenu(title: "Word Counter Bar Menu")
         statusBarItem.menu = statusBarMenu
+        let quitAppMenuItem = NSMenuItem(title: "Quit", action: #selector(quitApp), keyEquivalent: "")
+        statusBarMenu.addItem(quitAppMenuItem)
         
-        statusBarMenu.addItem(withTitle: "Count Words", action: nil, keyEquivalent: "")
+        let options: NSDictionary = [kAXTrustedCheckOptionPrompt.takeUnretainedValue() as String : true]
+        let accessEnabled = AXIsProcessTrustedWithOptions(options)
+        
+        print("Access\(accessEnabled ? "" : " Not") Enabled")
+        
+        // initialize array of events to store keystrokes
+        var eventArray = [NSEvent]()
+        
+        NSEvent.addGlobalMonitorForEvents(matching: NSEvent.EventTypeMask.keyDown) { (event) in
+            
+            if event.keyCode == 49 { // ignore spaces
+                // count the array as a word on the next space, if the array has contents in it
+                if eventArray.count > 1 { // basic check, needs to be more sophisticated
+                    self.currentWordCount+=1
+                    eventArray = []
+                }
+            } else {
+                eventArray.append(event) // this will include delete events, that's fine?
+            }
 
+            self.statusBarItem.button?.title = "\(self.currentWordCount!)"
+
+        }
     }
-
+    
+    func applicationWillTerminate(_ notification: Notification) {
+        UserDefaults.standard.set(self.currentWordCount, forKey: AppDelegate.currentWordCountKey)
+    }
+    
+    @objc func quitApp() {
+        NSApplication.shared.terminate(self)
+    }
 }
 
--- PlayingWithNSEvents/ViewController.swift
+++ PlayingWithNSEvents/ViewController.swift
@@ -13,53 +13,57 @@ class ViewController: NSViewController {
     @IBOutlet weak var activeApplicationTextField: NSTextField!
     @IBOutlet weak var wordCountTextField: NSTextField!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        let options: NSDictionary = [kAXTrustedCheckOptionPrompt.takeUnretainedValue() as String : true]
-        let accessEnabled = AXIsProcessTrustedWithOptions(options)
-        
-        print("Access\(accessEnabled ? "" : " Not") Enabled")
-        
-        // initialize word counter, v2 will be persisting this amount over a specified time range
-        wordCountTextField.stringValue = "0"
-        
-        // initialize array of events to store keystrokes
-        var eventArray = [NSEvent]()
-        
-        NSEvent.addGlobalMonitorForEvents(matching: NSEvent.EventTypeMask.keyDown) { (event) in
-            
-            print(event)
-            
-            // Get current word count
-            let numString = self.wordCountTextField.stringValue
-            
-            // Convert word count to int for addition
-            var numInt = Int(numString)!
-            
-            if event.keyCode == 49 { // ignore spaces
-                // count the array as a word on the next space, if the array has contents in it
-                if eventArray.count > 1 { // basic check, needs to be more sophisticated
-                    numInt+=1
-                    eventArray = []
-                }
-            } else {
-                eventArray.append(event) // this will include delete events, that's fine?
-            }
-            
-            self.activeApplicationTextField.stringValue = "You are currently typing in \(NSWorkspace.shared.frontmostApplication!.localizedName!)"
-            
-            /* a good start, but this program does not yet:
-             - sift through numbers, i wonder if there's a categorization for those keycodes
-             - if the user types 3 characters, then deletes those 3 characters, there will be six items in the eventArray. Which would count it as a word.
-             */
-            
-            self.wordCountTextField.stringValue = "\(numInt)"
-        }
-        
-        detectRunningApps()
-    
-    }
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        
+//        let options: NSDictionary = [kAXTrustedCheckOptionPrompt.takeUnretainedValue() as String : true]
+//        let accessEnabled = AXIsProcessTrustedWithOptions(options)
+//        
+//        print("Access\(accessEnabled ? "" : " Not") Enabled")
+//        
+//        // initialize word counter, v2 will be persisting this amount over a specified time range
+//        wordCountTextField.stringValue = "0"
+//        
+//        // initialize array of events to store keystrokes
+//        var eventArray = [NSEvent]()
+//        
+//        NSEvent.addGlobalMonitorForEvents(matching: NSEvent.EventTypeMask.keyDown) { (event) in
+//            
+//            print(event)
+//            
+//            // Get current word count
+//            let numString = self.wordCountTextField.stringValue
+//            
+//            // Convert word count to int for addition
+//            var numInt = Int(numString)!
+//            
+//            if event.keyCode == 49 { // ignore spaces
+//                // count the array as a word on the next space, if the array has contents in it
+//                if eventArray.count > 1 { // basic check, needs to be more sophisticated
+//                    numInt+=1
+//                    eventArray = []
+//                }
+//            } else {
+//                eventArray.append(event) // this will include delete events, that's fine?
+//            }
+//            
+//            self.activeApplicationTextField.stringValue = "You are currently typing in \(NSWorkspace.shared.frontmostApplication!.localizedName!)"
+//            
+//            /* a good start, but this program does not yet:
+//             - sift through numbers, i wonder if there's a categorization for those keycodes
+//             - if the user types 3 characters, then deletes those 3 characters, there will be six items in the eventArray. Which would count it as a word.
+//             */
+//            
+//            self.wordCountTextField.stringValue = "\(numInt)"
+//            
+//            if let appDelegate = NSApplication.shared.delegate {
+////                appDelegate.status
+//            }
+//        }
+//        
+//        detectRunningApps()
+//    
+//    }
     
     func detectRunningApps() {
         let apps = NSWorkspace.shared.runningApplications
